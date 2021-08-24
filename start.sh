da3b585555ec424ac8847878c36fdfc5b29dfd36
invinci-2 <88006981+invinci-2@users.noreply.github.com>
2021-08-24 15:08:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-pip3 install --upgrade yt-dlp
+pip3 -qq install --upgrade yt-dlp
 gunicorn wserver:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d --webui-port=8090 & ./aria.sh; python3 -m bot 
