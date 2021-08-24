a377c4340ad537ae8d787c25ce27cbfb822f8ef4
invinci-2 <88006981+invinci-2@users.noreply.github.com>
2021-08-24 15:06:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
-if [[ -n $TOKEN_PICKLE_URL ]]; then
-	wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
-fi
-
-if [[ -n $ACCOUNTS_ZIP_URL ]]; then
-	wget -q $ACCOUNTS_ZIP_URL -O /usr/src/app/accounts.zip
-	unzip accounts.zip -d /usr/src/app/accounts
-	rm accounts.zip
-pip3 -qq install --upgrade yt-dlp
+pip3 install --upgrade yt-dlp
 gunicorn wserver:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d --webui-port=8090 & ./aria.sh; python3 -m bot 
